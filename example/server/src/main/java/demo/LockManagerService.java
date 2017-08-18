b94aed1af8403b205e95cbd34390425e8840ce75
David Robison <david.robison@psgglobal.net>
2017-08-18 11:19:39 -0400

    Example code

--- example/server/src/main/java/demo/LockManagerService.java
+++ example/server/src/main/java/demo/LockManagerService.java
@@ -1,7 +1,8 @@
 package demo;
 
-import demo.LockManagerReactor;
+import javax.websocket.server.ServerEndpoint;
 
+@ServerEndpoint(value = "/VideoBroker/Recorder")
 public class LockManagerService extends LockManagerReactor<LockManagerSession> {
 
 }
